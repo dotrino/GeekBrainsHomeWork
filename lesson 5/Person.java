3b587b2a7457103746e8946011845b6243bda315
Игорь <dotrino@mail.ru>
2020-11-04 23:37:01 +0300

    домашнее задание по 5 лекции

---
+++ lesson 5/Person.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Person {
+
+    private String fio;
+    private String post;
+    private String email;
+    private String tel;
+    private double salary;
+    private int age;
+
+    Person(String fio, String post, String email, String tel, double salary, int age) {
+        this.fio = fio;
+        this.post = post;
+        this.email = email;
+        this.tel = tel;
+        this.salary = salary;
+        if(age >= 18) {
+            this.age = age;
+        } else {
+            System.out.println("Сотрудник должен быть старше 18 лет");
+        }
+    }
+
+    int GetAge() {
+        return age;
+    }
+
+    void Show() {
+        System.out.println(fio + " " + post + " " +  email + " " + tel + " " + salary + " " + age);
+    }
+}
+
+
