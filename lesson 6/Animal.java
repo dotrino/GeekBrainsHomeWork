2d714e3de3beb18fdbec780f280d7608c65bb55a
Игорь <dotrino@mail.ru>
2020-11-08 17:59:14 +0300

    домашнее задание  к уроку 6

---
+++ lesson 6/Animal.java
@@ -0,0 +1,10 @@
+abstract class Animal {
+
+    private final int RunLength = 0;
+    private final int SwimLength = 0;
+    private final double JumpHeight = 0;
+
+    abstract void run(int length);
+    abstract void swim(int length);
+    abstract void jump(double height);
+}
